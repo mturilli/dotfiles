835f3cc9edc89f6563e6b37856116756c85c3b1c
Matteo Turilli <matteo.turilli@gmail.com>
2025-08-10 06:07:02 +0200

    Fix outdated venv plugin

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -422,7 +422,12 @@ require("lazy").setup({
   -- Python virtual environments (updated to 2024 version)
   {
     "linux-cultist/venv-selector.nvim",
-    dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim" },
+    branch = "regexp",
+    dependencies = {
+        "neovim/nvim-lspconfig",
+        "nvim-telescope/telescope.nvim",
+        "mfussenegger/nvim-dap-python",
+    },
     config = function()
       require("venv-selector").setup({
         name = {
@@ -431,8 +436,6 @@ require("lazy").setup({
           "env",
           ".env",
         },
-        -- Use the new 2024 version
-        stay_on_this_version = false,
         -- Add user-defined search patterns for better venv detection
         search_venv_manager = {
           "poetry",
